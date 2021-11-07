778434a10bd4d69c44c7308d1aeb32990273dd84
nickjfenton <nickjfenton@yahoo.co.uk>
2021-11-07 18:53:51 +0000

    Initial chatter framework, docs

--- chatter/chat_client.py
+++ chatter/chat_client.py
@@ -1,15 +1,39 @@
-from chatter.queue import Subscriber, Message
+from abc import abstractmethod, ABC
+from typing import Callable
+
+from chatter.message import Message
 
 
-class ChatClient(Subscriber):
+class ChatClient(ABC):
+    """
+    A :class:`ChatClient` handles receiving messages from a chat system,
+    (typically through registering some callbacks), transforming these into
+    :class:`~chatter.message.Message` format for the chat bot to understand,
+    and forwarding these on.
 
+    Any subclass of `ChatClient` must implement two methods:
+
+    * `send_to_client`: allow the bot to send messages to the chat client
+    * `start_listening`: allow the bot to begin listening to the chat client
+    """
+
+    send_to_bot: Callable[[Message], None]
+    """A method bound to the :class:`~chatter.bot.Bot` that handles
+    sending messages to it."""
+
+    @abstractmethod
     def send_to_client(self, message: Message):
         """
-        Send messages to a chat client.
+        Send a message to the chat client.
 
-        The conversion of the message into a suitable format for the chat
+        Converting the message into a suitable format for the chat
         client is up to the implementing class.
         """
 
-    def send_to_features(self, message: Message):
-        pass
+    @abstractmethod
+    def start_listening(self) -> None:
+        """
+        Begin listening to the chat client for messages.
+
+        To forward a message from the chat client to the bot,
+        """
---
+++ docs/index.rst
@@ -0,0 +1,12 @@
+.. chatter documentation master file, created by
+   sphinx-quickstart on Sun Nov  7 18:47:57 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+.. include:: ../README.md
+   :parser: myst_parser.sphinx_
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
